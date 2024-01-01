9d4ed9d1fcc08417bf8693f43e2a9f37438e267e
Yonaiker Ocando <yonaiker2410@gmail.com>
2024-01-01 18:08:21 -0400

    guardamos todo el estado en localstorage

--- src/components/context/GlobalState.jsx
+++ src/components/context/GlobalState.jsx
@@ -1,5 +1,5 @@
 //hook para crear contexto createContext y usar un contexto  useContext
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 //inicializamos el estado que va a manejar el reducer
@@ -18,7 +18,19 @@ export const useGlobalStatae = () => {
 
 export const GlobalProvider = ({ children }) => {
   //usaremos useReducer para crear funciones que actulicen el estado global, esto es muy parecido a un useState solo que le pasamos "AppReducer" que es el archivo que va a tner la logica  y el "initialState" que es el estado inicial
-  const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(AppReducer, initialState, () => {
+    //buscamos en el local storage y guardamos lo que tengamos en localData
+    const localData = localStorage.getItem("transactions");
+
+    //si viene algo en localData lo convertimos en un arreglo, caso contrario iniciamos en un arreglo vacio
+    return localData ? JSON.parse(localData) : initialState;
+  });
+
+  //usamos useEffect para actualizar el estado cada vez que este cambie
+  useEffect(() => {
+    //convertir el state a json y guardarlo en localstorage
+    localStorage.setItem("transactions", JSON.stringify(state));
+  }, [state]);
 
   const addTransaction = (transaction) => {
     //en esta funcion le pasamos el dispatch que activa una accion de type, y el payload que es el contenido que en este caso es un objeto transaction que tiene description y monto
