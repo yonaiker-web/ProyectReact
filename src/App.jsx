0219b42359830f2f1d142fe0a8a82c92d56bc375
yonaiker <yonaiker.ocando@unidigital.global>
2024-01-02 09:15:38 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     //englobamos todo lo demas en el GlobalProvider para que todo los children compartan la misma informacion
     <GlobalProvider>
       <div className="p-10  ">
-        <div className="p-5  bg-slate-800 rounded-3xl flex gap-4  justify-between ">
+        <div className="p-5  bg-slate-800 rounded-3xl flex gap-4  justify-around ">
           <div className="">
             <Header />
             <Balance />
